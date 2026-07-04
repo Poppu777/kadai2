f4c312218144bc02d35a29584d19e35e584f9f12
Poppu777 <zuoyehuangdian@gmail.com>
2026-07-04 16:04:42 +0900

    三十分前

--- kadai2/Turn.h
+++ kadai2/Turn.h
@@ -1,9 +1,19 @@
 #pragma once
-
+#include <iostream>
 class Turn
 {
 	int t = (int)time(nullptr);
 	int Atk = t % 21;
+	while (true)
+	{
+		if (Slime < 0)
+		{
+			delete Slime;
+		}
+
+		cunt++;
+	}
+	std::cost << "ƒ^[ƒ“”" << count << std::endl;
 
 };
 
--- kadai2/kadai2.cpp
+++ kadai2/kadai2.cpp
@@ -5,8 +5,8 @@
 
 int max_val = 100;
 int min_val = 50;
-int get_rand(int min_val, int max_val) {
-	return (int((get_rand() % (max_val + 1 - min_val)) + min_val);
+int get_random(int min_val, int max_val) {
+	return (int((get_random() % (max_val + 1 - min_val)) + min_val);
 }
 
 int main()
@@ -14,38 +14,38 @@ int main()
 	{
 		Character Player;
 		Player.name_ = "プレイヤー";
-		Player.HP_ = get_rand;
+		Player.HP_ = get_random;
 	}
 
 	{
 		Character Slime;
 		Slime.name_ = "スライム";
-		Slime.HP_ = get_rand;
+		Slime.HP_ = get_random;
 	}
 
 	{
 		Character Goblin;
 		Goblin.name_	= "ゴブリン";
-		Goblin.HP_		=  get_rand;
+		Goblin.HP_		=  get_random;
 	}
 
 
 	{
 		Character* pPlayer = new Character;
 		pPlayer->neme_	= "プレイヤー";
-		pPlayer->HP_	= get_rand;
+		pPlayer->HP_	= get_random;
 	}
 
 	{
 		Character* pSlime = new Character;
 		pSlime->neme_	= "スライム";
-		pSlime->HP_		= get_rand;
+		pSlime->HP_		= get_random;
 	}
 
 	{
 		Character* pGoblin = new Character;
 		pGoblin->neme_	= "ゴブリン";
-		pGoblin->HP_	= get_rand;
+		pGoblin->HP_	= get_random;
 	}
 
 	while (true)
